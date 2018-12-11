70cb095b0aa10956977878b17e2ee8e08210e5e3
andypun <apun6146@bths.edu>
2018-12-10 22:09:44 -0500

    Final

---
+++ src/AndyPun/Drink.java
@@ -0,0 +1,19 @@
+package AndyPun;
+
+public class Drink implements MenuItem {
+    private String name;
+    private double price;
+    public Drink(double price, String name) {
+        this.name = name;
+        this.price = price;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public double getPrice() {
+        return price;
+    }
+}
+
---
+++ src/AndyPun/Salad.java
@@ -0,0 +1,18 @@
+package AndyPun;
+
+public class Salad implements MenuItem {
+    private String name;
+    private double price;
+    public Salad(double price, String name) {
+        this.name = name;
+        this.price = price;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public double getPrice() {
+        return price;
+    }
+}
